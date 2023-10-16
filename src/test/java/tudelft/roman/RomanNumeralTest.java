c18cd637562d7bd5b829ab79ccd414ab772cdd31
Amy Kuo <amykuo@Amys-MacBook-Pro-2.local>
2023-10-16 13:26:54 -0700

    lesson 2

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -1,36 +1,72 @@
 package tudelft.roman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
+    private RomanNumeral roman;
+
+    @BeforeEach
+    public void initialize() {
+        this.roman = new RomanNumeral();
+    }
 
     @Test
     public void singleNumber() {
-        RomanNumeral roman = new RomanNumeral();
+        //RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("I");
         Assertions.assertEquals(1, result);
     }
 
     @Test
     public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
+        //RomanNumeral roman = new RomanNumeral(); // the class I want to test
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
+        //RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("XLIV");
-        Assertions.assertEquals(44, result);
+        //RomanNumeral roman = new RomanNumeral();
+        int result = roman.convert("LXVI");
+        Assertions.assertEquals(66, result);
+
     }
+
+//    @Test
+//    public void singleNumber() {
+//        RomanNumeral roman = new RomanNumeral();
+//        int result = roman.convert("I");
+//        Assertions.assertEquals(1, result);
+//    }
+
+//    @Test
+//    public void numberWithManyDigits() {
+//        RomanNumeral roman = new RomanNumeral();
+//        int result = roman.convert("VIII");
+//        Assertions.assertEquals(8, result);
+//    }
+
+//    @Test
+//    public void numberWithSubtractiveNotation() {
+//        RomanNumeral roman = new RomanNumeral();
+//        int result = roman.convert("IV");
+//        Assertions.assertEquals(4, result);
+//    }
+//
+//    @Test
+//    public void numberWithAndWithoutSubtractiveNotation() {
+//        RomanNumeral roman = new RomanNumeral();
+//        int result = roman.convert("XLIV");
+//        Assertions.assertEquals(44, result);
+//    }
 }
